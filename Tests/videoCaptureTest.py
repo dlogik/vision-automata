6d55f845256844bc1d35cfb5064debf1b6666ac5
dlogik <dlogik@gmail.com>
2015-03-31 20:03:28 +1100

    * Added IPython notebook with high level project ideas. * Python test for capturing video in realtime.

---
+++ Tests/videoCaptureTest.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+import time as t
+
+
+cap = cv2.VideoCapture(0)
+
+frames = 0
+
+start = t.time()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+
+    diff = t.time() - start
+    frames += 1
+    print "{0} {1}".format(diff, (frames / diff))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
